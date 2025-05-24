a397b7d3a49e031652945ceb37bafcc243c894b7
Ratamacue9112 <130893701+Ratamacue9112@users.noreply.github.com>
2025-05-24 17:42:12 +0100

    Fix console taking ui_cancel when toggle_debug exists

--- addons/copper_dc/scripts/debug_console.gd
+++ addons/copper_dc/scripts/debug_console.gd
@@ -88,7 +88,7 @@ func _process(delta):
 
 func _input(event):
 	var open_debug_pressed = event.is_action_pressed("open_debug") if InputMap.has_action("open_debug") else false
-	var close_debug_pressed = event.is_action_pressed("close_debug") if InputMap.has_action("close_debug") else event.is_action_pressed("ui_cancel")
+	var close_debug_pressed = event.is_action_pressed("close_debug") if InputMap.has_action("close_debug") else (event.is_action_pressed("ui_cancel") if not InputMap.has_action("toggle_debug") else false)
 	var toggle_debug_pressed = event.is_action_pressed("toggle_debug") if InputMap.has_action("toggle_debug") else false
 	
 	# Open debug
