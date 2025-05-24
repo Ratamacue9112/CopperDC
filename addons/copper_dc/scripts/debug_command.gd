28249ca82ce4da19288aa3580f6679fc22adcefa
Ratamacue9112 <130893701+Ratamacue9112@users.noreply.github.com>
2025-05-24 20:58:57 +0100

    Fix inconsistent code formatting

--- addons/copper_dc/scripts/debug_command.gd
+++ addons/copper_dc/scripts/debug_command.gd
@@ -3,17 +3,17 @@ class_name DebugCommand
 var id: String
 var parameters: Array
 var function: Callable
-var functionInstance: Object
-var helpText: String
-var getFunction
+var function_instance: Object
+var help_text: String
+var get_function
 
-func _init(id:String, function:Callable, functionInstance: Object, parameters:Array=[], helpText:String="", getFunction=null):
+func _init(id:String, function:Callable, function_instance: Object, parameters:Array=[], help_text:String="", get_function:Callable=Callable()):
 	self.id = id
 	self.parameters = parameters
 	self.function = function
-	self.functionInstance = functionInstance
-	self.helpText = helpText
-	if getFunction != null: self.getFunction = getFunction
+	self.function_instance = function_instance
+	self.help_text = help_text
+	if get_function != Callable(): self.get_function = get_function
 
 class Parameter:
 	var name: String
